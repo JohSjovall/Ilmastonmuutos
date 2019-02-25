2f9ecbeac9c7112f29763d67b5a83a9a4beed3d5
JohanS <joalsj@utu.fi>
2019-02-25 20:24:10 +0200

    putki ongelma

--- CSV.js
+++ CSV.js
@@ -8,18 +8,10 @@ var red = fs.createReadStream('API_EN.ATM.CO2E.KT_DS2_en_csv_v2_10473877.csv')
 .pipe(csv()).on('data',function(data){
     list.push(data);
     //console.log(data[1]);
-}).on('end', function(){
+}).on('end', function(listaa){
     console.log('Read finished');
     for (var i = 0; i < list.length; i++){
-        console.log(i+": "+list[i][0]);
-        return(
-            <div>
-                <taple>
-
-                </taple>
-            </div>
-        )
+        console.log(i+": "+list[i]);
     }
-    
 });
 console.log("stream");
--- server.js
+++ server.js
@@ -7,5 +7,41 @@ app.listen(port, function() {
 });
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.send(list());
 })
+
+
+function list() {
+    var fs = require('fs');
+    var csv = require('fast-csv');
+
+    //const data = "";
+
+    const list = [];
+    function red() {
+        fs.createReadStream('API_EN.ATM.CO2E.KT_DS2_en_csv_v2_10473877.csv')
+        .pipe(csv()).on('data',function(data){
+            list.push(data);
+            //console.log(data[1]);
+        }).on('end', function(){
+            var text = ""
+            //console.log('Read finished');
+            for (var i = 4; i < list.length; i++){
+                var arr=list[i];
+                //console.log(arr)
+                text +="<tr>"
+                //console.log(i+": "+list[i]);
+                arr.forEach(y => text+="<td>"+y+"<td />");
+                text +="</tr>"
+                //var doc = document.getElementsByTagName('table')[0].innerHTML=text;
+                //console.log(text);
+            }
+            //console.log(text);
+            console.log('Read finished');
+            return text
+        }).unpipe(list);
+    }
+    console.log(red());
+    console.log(list);
+    return red();
+}
